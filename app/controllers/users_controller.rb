fc3ebc7155ab8ce76577ab312a7e03a69535cd1b
eaguidigodo <eaguidigodo@gmail.com>
2021-09-07 13:47:55 +0100

    Register feature implemented

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(set_params)
+        if @user.save()
+            redirect_to posts_index_path, notice: "Your account has been succesfully created!"
+        else
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+    def set_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
