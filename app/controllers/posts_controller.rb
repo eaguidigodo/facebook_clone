3f6ddfc90c45056092e5d371d3316b0e048b11a1
eaguidigodo <eaguidigodo@gmail.com>
2021-09-08 17:55:15 +0100

    Implemented edit post  function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update]
   def index
     @post = Post.new
     @posts = Post.all
@@ -17,8 +18,24 @@ class PostsController < ApplicationController
     @post = current_user.posts.build(post_params)
   end
 
+  def edit
+    @posts = Post.all
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_index_path, notice: "Your post has been succesfully updated!"
+    else
+      render :edit
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   get 'posts/index'
-  post 'posts/confirm'
-  resources :posts
+  resources :posts do
+    collection do
+      post :confirm 
+    end
+  end
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
