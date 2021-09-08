a21c26bb4a4ed28f360d45e69b654331bc1c1f65
eaguidigodo <eaguidigodo@gmail.com>
2021-09-08 15:55:39 +0100

    Implemented posting function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.new
+  end
+
+  def create
+    raise
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_index_path, notice: "Your post has succesfully been created!"
+    else
+      render :index
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
